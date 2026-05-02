fb7277f094c1b54a34143daad4ac442b5257a0fd
Jakab Gergo <jakabgergo23@gmail.com>
2026-05-02 15:22:11 +0300

    preparation to publish to npm

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/image-viewer.js",
+      name: "ImageViewer",
+      fileName: "image-viewer",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["lit"], // don't bundle lit, let the consumer provide it
+    },
+  },
   server: {
     proxy: {
       "/api": "http://localhost:3001",
